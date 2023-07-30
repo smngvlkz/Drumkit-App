6a05281f08b07e137b99c5b3d95d9e02d12f664f
TheCodingPantsula <asmdigital14@gmail.com>
2023-07-30 13:07:24 +0100

    adding more features and sounds to the app

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+
+
+
+
+
+
 // Retrieve the kick button element
 const kickButton = document.getElementById("kick");
+const clapsButton = document.getElementById("claps");
 
 // Retrieve the kick sound element
 const kickSound = document.getElementById("kick-sound");
+const clapsSound = document.getElementById("claps-sound");
 
 // Attach a click event listener to the kick button
 kickButton.addEventListener("click", function() {
@@ -12,3 +20,11 @@ kickButton.addEventListener("click", function() {
   // Play the kick sound
   kickSound.play();
 });
+
+clapsButton.addEventListener("click", function() {
+    // Reset the sound time to replay it
+    clapsSound.currentTime = 0;
+    
+    // Play the kick sound
+    clapsSound.play();
+  });
